a2fc463a509f7c9a6bbf38a66c864ad3b7148df6
winghou <winghou@tencent.com>
2021-05-30 18:24:47 +0800

    新增文件

---
+++ src/main/java/com/springboot/start/entity/UserEntity.java
@@ -0,0 +1,14 @@
+package com.springboot.start.entity;
+
+import lombok.Data;
+
+@Data
+public class UserEntity {
+
+  private String age;
+
+  private String name;
+
+  private String sex;
+
+}
---
+++ src/main/java/com/springboot/start/service/UserService.java
@@ -0,0 +1,8 @@
+package com.springboot.start.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
